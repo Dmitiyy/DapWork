c3d9f10317ac08eef5b8adec18c987a328a64930
Dmitriy <dmitriyy311@gmail.com>
2022-03-30 17:26:23 +0300

    added right sidebar

---
+++ src/app/home/home-top/home-top.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-home-top',
+  templateUrl: './home-top.component.html',
+  styleUrls: ['./home-top.component.sass']
+})
+export class HomeTopComponent {};
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeNavComponent } from './home-nav/home-nav.component';
+import { HomeTopComponent } from './home-top/home-top.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HomeNavComponent
+    HomeNavComponent,
+    HomeTopComponent
   ],
   imports: [
     CommonModule,
