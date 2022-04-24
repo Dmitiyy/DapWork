acd5bb48f8a37b52347f85bb4ae84ca750f61d86
Dmitriy <dmitriyy311@gmail.com>
2022-04-24 12:48:53 +0300

    created profile markup

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-profile',
-  templateUrl: 'profile.component.html'
+  templateUrl: 'profile.component.html',
+  styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent {};
